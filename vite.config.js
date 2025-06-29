8d85c19de8f509e26cca01e92c3b79cd7f3f658c
GhodjeezReal <fredrickokojie@yahoo.com>
2025-06-29 16:57:34 +0100

    Set up Vite PWA plugin and update config for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'My Todo App',
+        short_name: 'Todo',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#070707',
+        theme_color: '#070707',
+        description: 'A simple todo app with offline support',
+        icons: [
+          {
+            src: 'img/icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'img/icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
